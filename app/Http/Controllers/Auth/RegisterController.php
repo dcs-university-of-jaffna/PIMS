7407f73f3edff60b7aa492172ec44a4b2d8f0c80
ChamikaraTDP <pramudithachamikara@gmail.com>
2019-07-27 21:30:03 +0530

    some minor changes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'is_admin'=> 0,
             'password' => Hash::make($data['password']),
         ]);
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,29 +15,29 @@
 </head>
 <body>
   <br>
-  
+
   <div class="container">
-      
+
     <h1 >Poison Details</h1>
   </div>
-    
-   
+
+
     <div class="container containeradd">
-       
+
         <ul class="nav nav-tabs toggle">
           <li class="active"><a data-toggle="tab" href="#Natural_Toxins">Natural Toxins</a></li>
           <li><a data-toggle="tab" href="#Chemical">Chemical</a></li>
           <li><a data-toggle="tab" href="#Pharmaceutical">Pharmaceutical</a></li>
           <li><a data-toggle="tab" href="#Others">Others</a></li>
         </ul>
-      
+
         <div class="tab-content">
           <div id="Natural_Toxins" class="tab-pane fade in active">
 
        <div class="row">
-            
+
           <div class="column" style="background-color:rgb(245, 245, 245);">
-          <h3>Fauna</h3> 
+          <h3>Fauna</h3>
                  <ul class="font">
                     <li  ><a href="#">Snake bite</a><br></li>
                     <li><a href="#">Scorpion bite</a><br></li>
@@ -49,7 +52,7 @@
                     <li><a href="#">Others</a><br></li>
                 </ul>
            </div>
-       <div class="column" style="background-color:rgb(235, 235, 235);">     
+       <div class="column" style="background-color:rgb(235, 235, 235);">
            <h3 >Flora</h3>
                 <ul class="font">
                     <li><a href="#">Allary</a><br></li>
@@ -63,43 +66,44 @@
                     <li><a href="#">Endana</a><br></li>
                     <li><a href="#">Diya Kadunu</a><br></li>
                     <li><a href="#">Others</a><br></li>
-                </ul> 
+                </ul>
            </div>
-        <div class="column" style="background-color:rgb(245, 245, 245);">         
-            
-             <h3>Microbial Poison</h3>  
+        <div class="column" style="background-color:rgb(245, 245, 245);">
+
+             <h3>Microbial Poison</h3>
                 <ul class="font">
                     <li><a href="#">Botalism</a></li>
                     <li><a href="#">Food poisonings</a></li>
                 </ul>
         </div>
 
-        <div class="column" style="background-color:rgb(235, 235, 235);">  
-               
+        <div class="column" style="background-color:rgb(235, 235, 235);">
+
                 <h3>Flora</h3>
                  <ul class="font">
                     <li><a href="#">Mushroom</a></li>
                  </ul>
-            </div>  
-              
-                </div>   
+            </div>
+
+                </div>
           </div>
           <div id="Chemical" class="tab-pane fade">
             <h3>Chemical 2</h3>
-            
+
           </div>
           <div id="Pharmaceutical" class="tab-pane fade">
             <h3>Pharmaceutical</h3>
-            
+
           </div>
           <div id="Others" class="tab-pane fade">
             <h3>Others</h3>
-            
+
           </div>
         </div>
       </div>
-      
-     
+
+
 
 </body>
 </html>
+@endsection
