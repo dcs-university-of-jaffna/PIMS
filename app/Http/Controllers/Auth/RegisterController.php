6db61794e4cd75c3a9ff164caae76a71a225adbc
ChamikaraTDP <pramudithachamikara@gmail.com>
2019-07-27 22:20:45 +0530

    adde position to RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'is_admin'=> 0,
+            'position'=> 'user',
             'password' => Hash::make($data['password']),
         ]);
     }
