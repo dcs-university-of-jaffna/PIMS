9117e028e9679787abd15011a915e5a287379d03
DilshanDilipudara <gunawardhanaudara@gmail.com>
2019-07-28 00:10:25 +0530

    migate Clinical table

---
+++ database/migrations/2019_07_27_181943_clinicals.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Clinicals extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Clinicals', function (Blueprint $table) {
+           
+            $table->bigIncrements('Clinicals_Id');
+            $table->tinyInteger('Burning_Pain');
+            $table->tinyInteger('Pruritus');
+            $table->tinyInteger('Vomiting');
+            $table->tinyInteger('Bronchospasm');
+            $table->tinyInteger('Renal_Failure');
+            $table->tinyInteger('Tightness_of_Chest');
+            $table->tinyInteger('Urticaria');
+            $table->tinyInteger('Laryngeal_Odema');
+            $table->tinyInteger('Rhabdomyolysis');
+            $table->tinyInteger('Swelling');
+            $table->tinyInteger('Nausea');
+            $table->tinyInteger('Hypotension');
+            $table->tinyInteger('Oliguria');
+            $table->tinyInteger('Diarrhoea');
+            $table->tinyInteger('Malaise');
+            $table->tinyInteger('Facial_Odema');
+            $table->tinyInteger('Seizure');    
+            $table->timestamps();
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Clinicals');
+    }
+}
