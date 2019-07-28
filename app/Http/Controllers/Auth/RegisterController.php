6d27053d6fea84c481fd289583bc5b4095629e9b
ChamikaraTDP <pramudithachamikara@gmail.com>
2019-07-28 20:08:23 +0530

    rollback user.php changes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,6 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'position'=> 'user',
             'password' => Hash::make($data['password']),
         ]);
     }
