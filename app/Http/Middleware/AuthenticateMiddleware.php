a7345e09dd78348212c188a0321b2472497561e1
Thusha <30925366+Thushacsc@users.noreply.github.com>
2019-07-29 01:27:29 +0530

    newly Add Authenticate middle ware file into middleware folder

---
+++ app/Http/Middleware/AuthenticateMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class AuthenticateMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check())
+        {
+            if($request->User()->userType=='Admin')
+            {
+               return $next($request);
+            }
+             return redirect('/');    
+        }
+
+        return redirect('/login');
+
+        /*if(Auth::check()){
+            return $next($request);
+        }
+        else{
+            return redirect('/login');
+        }*/
+    }
+}
