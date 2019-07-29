1f1ddb9275f3220923149832090d1d996472fa40
Thushacsc <thushabala1996@gmail.com>
2019-07-29 11:33:02 -0400

    add admin route for admin panel

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,16 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::post('/submit', 'BeeStringController@submit');
+
+
+Route::group(['middleware' => ['App\Http\Middleware\AuthenticateMiddleware']], function () {
+    //admin routes
+    Route::get('/Active/{id}','SetUserPermissionController@Active')->middleware('AuthenticateMiddleware');
+    Route::get('/notActive/{id}','SetUserPermissionController@notActive')->middleware('AuthenticateMiddleware');
+    Route::get('/Admin/{id}','SetUserPermissionController@Admin')->middleware('AuthenticateMiddleware');
+    Route::get('/notAdmin/{id}','SetUserPermissionController@notAdmin')->middleware('AuthenticateMiddleware');
+    Route::get('/userPermission/addnew','SetUserPermissionController@create')->middleware('AuthenticateMiddleware');
+    Route::post('/userPermission','SetUserPermissionController@store')->middleware('AuthenticateMiddleware');
+    Route::delete('/userPermission/{id}','SetUserPermissionController@destroy')->middleware('AuthenticateMiddleware');
+    Route::resource('/userPermission','SetUserPermissionController')->middleware('AuthenticateMiddleware');
+});
