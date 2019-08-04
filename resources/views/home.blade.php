60232de5ff157611ace119fa5f1740d69e8aa5e0
DilshanDilipudara <gunawardhanaudara@gmail.com>
2019-08-04 10:11:00 +0530

    change home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,146 +1,206 @@
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Poison</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="/poisonStyles.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
+    <link rel="stylesheet" href="https://raw.githubusercontent.com/daneden/animate.css/master/animate.css">
+    <link rel="stylesheet" href="/Homepage/css/bootnavbar.css">
+
+    <title>PIMS</title>
+    <meta name="description" content="Multi level hover dropdown Navbar for bootstrap 4">
+    <meta name="keywords" content="Multi level hover dropdown Navbar for bootstrap 4">
 </head>
+
 <body>
-  <br>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" id="main_navbar">
+        <a class="navbar-brand" href="#">PIMS</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-<div class="container">
-  <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">PHN</a>
-    </div>
+        <div class="container collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        Natural Toxins
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      
+                        <li class="nav-item dropdown">
+                                <a class="dropdown-item dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                     Fauna &nbsp;&nbsp;&nbsp;
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                                    <li><a class="dropdown-item" href="{{ url('/form') }}">Bee Sting</a></li>
+                                    <li><a class="dropdown-item" href="#">Black widow spider</a></li>  
+                                    <li><a class="dropdown-item" href="#">Blister beetle</a></li> 
+                                    <li><a class="dropdown-item" href="#">Centipede</a></li> 
+                                    <li><a class="dropdown-item" href="#">Hornet sting</a></li> 
+                                    <li><a class="dropdown-item" href="#">Jellyfish</a></li> 
+                                    <li><a class="dropdown-item" href="#">Snake bite</a></li> 
+                                    <li><a class="dropdown-item" href="#">Scorpion bite</a></li> 
+                                    <li><a class="dropdown-item" href="#">Turtle flesh poisoning</a></li> 
+                                    <li><a class="dropdown-item" href="#">Wasp sting</a></li> 
+                                    <li><a class="dropdown-item" href="#">Others</a></li> 
+                    
+                                </ul>
+                            </li>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <li class="nav-item dropdown">
+                                <a class="dropdown-item dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                     Flora &nbsp;&nbsp;&nbsp;
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                                    <li><a class="dropdown-item" href="#">Allary</a></li>
+                                    <li><a class="dropdown-item" href="#">Aththana</a></li>
+                                    <li><a class="dropdown-item" href="#">Aththe</a></li>
+                                    <li><a class="dropdown-item" href="#">Diya Kadunu</a></li>
+                                    <li><a class="dropdown-item" href="#">Endana</a></li>
+                                    <li><a class="dropdown-item" href="#">Goda Kaduru</a></li>
+                                    <li><a class="dropdown-item" href="#">Habarala</a></li>
+                                    <li><a class="dropdown-item" href="#">Hondala</a></li>
+                                    <li><a class="dropdown-item" href="#">Karththigai</a></li>
+                                    <li><a class="dropdown-item" href="#">Kepunkiriya</a></li>
+                                    <li><a class="dropdown-item" href="#">Others</a></li>
+                   
+                                </ul>
+                            </li>
 
-      <form  action="/phnsearch" class="navbar-form navbar-left" method="POST">
-      {{csrf_field()}}
-        <div class="form-group">
-          <input type="number" min="0" class="form-control" placeholder="Search" name = "phn" required>
+                            <li class="nav-item dropdown">
+                                <a class="dropdown-item dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                    Microbial Poison
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                                    <li><a class="dropdown-item" href="#">Botalism</a></li>
+                                    <li><a class="dropdown-item" href="#">Food Poisonings</a></li>   
+                                </ul>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="dropdown-item dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                    Fungal Poison
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                                    <li><a class="dropdown-item" href="#">Mushroom</a></li>  
+                                </ul>
+                            </li>
+
+                    </ul>
+                </li>
+                
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        Chemical
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      
+                        <li class="nav-item dropdown">
+                                <a class="dropdown-item dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                     chemical
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                                    <li><a class="dropdown-item" href="#">chemical</a></li>
+                                    <li><a class="dropdown-item" href="#">chemical</a></li>   
+                                </ul>
+                            </li>
+                    </ul>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        Pharmaceutical
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      
+                        <li class="nav-item dropdown">
+                                <a class="dropdown-item dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                    Pharmaceutical
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                                    <li><a class="dropdown-item" href="#">Pharmaceutical</a></li>
+                                    <li><a class="dropdown-item" href="#">Pharmaceutical</a></li>   
+                                </ul>
+                            </li>
+                    </ul>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        Others
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      
+                        <li class="nav-item dropdown">
+                                <a class="dropdown-item dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                    Others
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                                    <li><a class="dropdown-item" href="#">Others</a></li>
+                                    <li><a class="dropdown-item" href="#">Others</a></li>   
+                                </ul>
+                            </li>
+                    </ul>
+                </li>
+
+
+
+
+            </ul>
+            
         </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
+           
+         <form class="form-inline my-2 my-lg-0"  action="/phnsearch" method="POST">
+         {{csrf_field()}}
+                <input class="form-control mr-sm-2"  type="number" min="0" name = "phn" placeholder="Search" aria-label="Search" required>
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+           </form>
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    
 
-      <ul class="nav navbar-nav navbar-right">
-      @if(Auth::user()->userType   =='Admin')
-        <li><a href="{{ url('/userPermission') }}">Admin</a></li>
-        @endif
-        <li class="nav-item"><a href="{{ route('logout') }}"  class="nav-link"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-          </li>
+       <ul class="nav navbar-nav navbar-right">
+            @if(Auth::user()->userType   =='Admin')
+              <li><a href="{{ url('/userPermission') }}">Admin</a></li>
+              @endif
+              <li class="nav-item"><button class="btn btn-outline-primary my-2 my-sm-0" href="{{ route('logout') }}"  class="nav-link"
+                                                  onclick="event.preventDefault();
+                                                          document.getElementById('logout-form').submit();">
+                                                  Logout
+                                     </button>
+                                              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                  {{ csrf_field() }}
+                                              </form>
+                </li>
       </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-</div>
-
-
-
-  <div class="container">
-
-    <h1 >Poison Details</h1>
-  </div>
-
-
-    <div class="container containeradd">
-
-        <ul class="nav nav-tabs toggle">
-          <li class="active"><a data-toggle="tab" href="#Natural_Toxins">Natural Toxins</a></li>
-          <li><a data-toggle="tab" href="#Chemical">Chemical</a></li>
-          <li><a data-toggle="tab" href="#Pharmaceutical">Pharmaceutical</a></li>
-          <li><a data-toggle="tab" href="#Others">Others</a></li>
-        </ul>
-
-        <div class="tab-content">
-          <div id="Natural_Toxins" class="tab-pane fade in active">
-
-       <div class="row">
-
-          <div class="column" style="background-color:rgb(245, 245, 245);">
-          <h3>Fauna</h3>
-                 <ul class="font">
-                    <li  ><a href="#">Snake bite</a><br></li>
-                    <li><a href="#">Scorpion bite</a><br></li>
-                    <li><a href="#">Jellyfish</a><br></li>
-                    <li><a href="{{ url('/form') }}">Bee sting</a><br></li>
-                    <li><a href="#">Hornet sting</a><br></li>
-                    <li><a href="#">Wasp sting</a><br></li>
-                    <li><a href="#">Black widow spider</a><br></li>
-                    <li><a href="#">Blister beetle</a><br></li>
-                    <li><a href="#">Centipede</a><br></li>
-                    <li><a href="#">Turtle flesh poisoning</a><br></li>
-                    <li><a href="#">Others</a><br></li>
-                </ul>
-           </div>
-       <div class="column" style="background-color:rgb(235, 235, 235);">
-           <h3 >Flora</h3>
-                <ul class="font">
-                    <li><a href="#">Allary</a><br></li>
-                    <li><a href="#">Aththana</a><br></li>
-                    <li><a href="#">Aththe</a><br></li>
-                    <li><a href="#">Karththigai</a><br></li>
-                    <li><a href="#">Kepunkiriya</a><br></li>
-                    <li><a href="#">Hondala</a><br></li>
-                    <li><a href="#">Habarala</a><br></li>
-                    <li><a href="#">Goda Kaduru</a><br></li>
-                    <li><a href="#">Endana</a><br></li>
-                    <li><a href="#">Diya Kadunu</a><br></li>
-                    <li><a href="#">Others</a><br></li>
-                </ul>
-           </div>
-        <div class="column" style="background-color:rgb(245, 245, 245);">
-
-             <h3>Microbial Poison</h3>
-                <ul class="font">
-                    <li><a href="#">Botalism</a></li>
-                    <li><a href="#">Food poisonings</a></li>
-                </ul>
-        </div>
-
-        <div class="column" style="background-color:rgb(235, 235, 235);">
-
-                <h3>Fungal Poison</h3>
-                 <ul class="font">
-                    <li><a href="#">Mushroom</a></li>
-                 </ul>
-            </div>
-
-                </div>
-          </div>
-          <div id="Chemical" class="tab-pane fade">
-            <h3>Chemical 2</h3>
-
-          </div>
-          <div id="Pharmaceutical" class="tab-pane fade">
-            <h3>Pharmaceutical</h3>
-
-          </div>
-          <div id="Others" class="tab-pane fade">
-            <h3>Others</h3>
-
-          </div>
-        </div>
-      </div>
-
-
-
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </nav>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="/Homepage/js/bootnavbar.js" ></script>
+    <script>
+        $(function () {
+            $('#main_navbar').bootnavbar();
+        })
+    </script>
 </body>
+
 </html>
