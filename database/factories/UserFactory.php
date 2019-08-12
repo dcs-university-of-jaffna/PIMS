01b61ee7a31ad6bfc68a1348a8379d35a5386916
ChamikaraTDP <pramudithachamikara@gmail.com>
2019-08-12 22:31:53 +0530

    Modified PatientFactory & UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,18 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/**
+ * Factory seeding the users table
+ * php version 7.2.19
+ *
+ * @var \Illuminate\Database\Eloquent\Factory $factory
+ *
+ * @category Factory
+ * @package  Database/factories
+ * @author   Group7s <dcs@jfn.ac.lk>
+ * @license  GNU General Public License (GPL)
+ * @link     https://github.com/dcs-university-of-jaffna/PIMS.git
+ */
+
 use App\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
@@ -19,8 +31,8 @@ use Faker\Generator as Faker;
 $factory->define(
     User::class, function (Faker $faker) {
         return [
-            'name' => $faker->name,
-            'email' => $faker->unique()->safeEmail,
+            'name' => 'Dr.'. $faker->firstName . ' '. $faker->lastName,
+            'email' => $faker->unique()->freeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
