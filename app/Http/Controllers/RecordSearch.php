8069916d075a3ee40f48b8e64f87dbe2757ecd3b
DilshanDilipudara <gunawardhanaudara@gmail.com>
2019-09-17 13:28:40 +0530

    remove bee String view data

--- app/Http/Controllers/RecordSearch.php
+++ app/Http/Controllers/RecordSearch.php
@@ -21,29 +21,7 @@ class RecordSearch extends Controller
             ->get();
          //   dd($data);
     return view('record',compact('data'));
-
-     
    }
-
-   
-   public function show_bee_sting_details($record_id,$phn){
-
-
-      $data = DB::table('records')
-            ->join('bee_stings','bee_stings.bee_stings_id','=','records.toxicity_id')
-            ->join('clinicals','clinicals.clinicals_id','=','records.clinicals_id')
-            ->join('managements','managements.managements_id','=','records.managements_id')
-            ->join('sites','sites.sites_id','=','records.sites_id')
-            ->where('records.records_id','=',$record_id)
-            ->get();
-      
-       return view('bee_String_view',compact('data'));
-   }
-
-
-
-
-
 
 
 }
