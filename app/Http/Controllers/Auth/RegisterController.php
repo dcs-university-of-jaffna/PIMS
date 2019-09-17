e256f60e8cc9f3a325a0e784a5a82af1cb4e1ed9
Thusha <30925366+Thushacsc@users.noreply.github.com>
2019-09-18 04:57:54 +0530

    I missed one input, now okay

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
