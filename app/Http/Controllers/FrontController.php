6328e7c0e4f2a5e9b89323bdf710892211205b61
DilshanDilipudara <gunawardhanaudara@gmail.com>
2019-09-19 19:20:24 +0530

    show the name in blade

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,19 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use DB;
+use App\Incident;
+
 
 class FrontController extends Controller
 {
 
     public function show_save_record()
     {
-       $data = DB::table('incidents')
-               ->join('patients','patients.id','=','incidents.patient_id')
-               ->join('toxicities','toxicities.id','=','incidents.toxicity_id')
-               ->where('incidents.is_submited','=',0)
-               ->get();
-       // dd($data);
+        $data = incident::where('is_submited',0)->latest()->paginate(15);
         return view('home',compact('data'));
 
     }
