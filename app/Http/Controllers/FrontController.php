2b13daf9c64fb9d00004c429ee095961989d6542
ChamikaraTDP <pramudithachamikara@gmail.com>
2019-09-19 16:54:36 +0530

    front controller edit

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Incident;
 use DB;
 
 class FrontController extends Controller
@@ -10,14 +10,8 @@ class FrontController extends Controller
 
     public function show_save_record()
     {
-       $data = DB::table('incidents')
-               ->join('patients','patients.id','=','incidents.patient_id')
-               ->join('toxicities','toxicities.id','=','incidents.toxicity_id')
-               ->where('incidents.is_submited','=',0)
-               ->get();
-       // dd($data);
-        return view('home',compact('data'));
-
+        $incidents = Incident::where('is_submited', 0)->latest()->paginate(10);
+        return view('home',compact('incidents'));
     }
 }
 
