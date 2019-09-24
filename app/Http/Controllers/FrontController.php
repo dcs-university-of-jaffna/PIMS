ae69122be5c9bdc10285b911bb88b8fd92eb333b
ChamikaraTDP <pramudithachamikara@gmail.com>
2019-09-24 20:20:29 +0530

    minor change

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -10,8 +10,8 @@ class FrontController extends Controller
 
     public function show_save_record()
     {
-        $incidents = Incident::where('is_submited', 0)->latest()->paginate(10);
-        return view('home',compact('incidents'));
+        $data = Incident::where('is_submited', 0)->latest()->paginate(10);
+        return view('home',compact('data'));
     }
 }
 
