dbd9777de237a7bba1589afd5f604197662939d0
DilshanDilipudara <gunawardhanaudara@gmail.com>
2019-10-01 03:30:07 +0530

    phn search Olinda controller and route

--- app/Http/Controllers/view_record_controller_phn_search.php
+++ app/Http/Controllers/view_record_controller_phn_search.php
@@ -146,9 +146,19 @@ class view_record_controller_phn_search extends Controller
                 // dd($incident);
                 return view('/flora_view/Niyagala',compact('incident','patient','toxicity','symptom','management','user'));
             } 
+  //Olinda
 
-
-
+            else if($request->toxicity_name == 'Olinda'){
+                                                                                                
+              $incident = Incident::find($request->incident_id);
+              $patient  = $incident->patient;
+              $toxicity = $incident->toxicity->natural->flora;
+              $symptom  = $incident->symptoms;
+              $management = $incident->managements();
+              $user    = $incident->users();                                          
+              // dd($incident);
+              return view('/flora_view/Olinda',compact('incident','patient','toxicity','symptom','management','user'));
+          } 
 
 
 
