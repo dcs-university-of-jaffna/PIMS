edd8bd3013f59de268e1f18c1cae8e833aa3e730
rayanaradha <rayanaradha1993@gmail.com>
2019-10-13 17:08:16 +0530

    finish the management part in fauna form design

--- resources/views/Detail_Forms/FaunaForm.blade.php
+++ resources/views/Detail_Forms/FaunaForm.blade.php
@@ -177,6 +177,39 @@
             
     <div data-panel="four">
 			<h4> Managemnt Detail</h4>
+                        
+                    <br> <input type="checkbox" name = "management[]" value="2"  > Adrenaline <br>
+                     <input type="checkbox" name = "management[]" value="4"  >  Analgesics given <br>
+                     <input type="checkbox" name = "management[]" value="41" > Antacids <br>
+                     <input type="checkbox" name = "management[]" value="6"  > Anticonvulsants given  <br>  
+                     <input type="checkbox" name = "management[]" value="8"  >  Antihistamine <br> 
+                     <input type="checkbox" name = "management[]" value="11" > Calcium gluconate given <br>
+		     <input type="checkbox" name = "management[]" value="16" > Gastric lavage <br>  
+                     <input type="checkbox" name = "management[]" value="33" > Hydrocortisone <br>
+		     <input type="checkbox" name = "management[]" value="36" > Hemodialysis <br>
+					 
+
+                     <label11>
+                            <input type="checkbox" name = "management[]" value="17" > Icepacks Applaid <br>   
+                            <input type="checkbox" name = "management[]" value="40"  > Inotropic support <br>
+                            <input type="checkbox" name = "management[]" value="34"  > Nebulisation<br>
+                            <input type="checkbox" name = "management[]" value="35"  > Peritoneal<br>                        
+                            <input type="checkbox" name = "management[]" value="31"  > Rest the area of bite<br>      
+                            <input type="checkbox" name = "management[]" value="26"  > Resuscitation <br>
+                            <input type="checkbox" name = "management[]" value="37"  > Tetanus prophylaxis <br>
+			    <input type="checkbox" name = "management[]" value="38"  > Topical steroids <br>
+			    <input type="checkbox" name = "management[]" value="30"  > Wound cleaning<br>     
+                     </label11> 
+                        <br><br>
+                        If others : <br> 
+                            <textarea rows = "3" cols = "120" name = "managements others"  placeholder="Enter other management detail here"></textarea>
+                        <br><br>
+                        
+                         <label9> <b> AVS :</b> </label><br><br>
+                            <input type="checkbox" name = "management[]" value="7"  > Anti snake venom serum(AVS) given<br>  
+                             If yes, no of vials :  
+                            <input type="number" name = "AVS" >
+                         </label9>
                      
     </div>
              
